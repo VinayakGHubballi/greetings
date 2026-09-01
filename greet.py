8c7edc19c1fa49f49311b92c52b85e13606b71a2
vinayak.g.hubballi <vinayak.g.hubballi@gmail.com>
2026-09-01 12:27:23 +0530

    modifed the file

--- greet.py
+++ greet.py
@@ -1,4 +1,11 @@
 greet = input("Enter your name to greet")
-print("Good morning", greet)
+
+
+fname = input("Enter your name")
+lname = input("Enter your lname")
+
+print("Good morning", fname+" "+lname)
 
 print("Second line with modifications")
+
+
